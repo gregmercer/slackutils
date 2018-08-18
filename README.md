b294ce0395e0f1493ca9064824d65308d34be982
Greg Mercer (GBear) <gmercer@stanford.edu>
2018-08-18 16:15:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# slackutils
+Command-line Nodejs Slack Utils using commander, inquirer, and colors
+
+```
+> git clone git@github.com:gregmercer/slackutils.git
+> slackutils
+> npm init -y
+> npm install --save commander inquirer colors axios
+```
+
+## To install app globally
+```
+> npm install -g ./
+```
+## Setup on Slack
+
+Go to here to create a token for Slack API calls
+```
+https://api.slack.com/custom-integrations/legacy-tokens
+```
+
+Set the CHANNELS_TOKEN as an environment variable
+```
+> export CHANNELS_TOKEN=<your slack token>
+
+Run the Slack Utilities
+```
+slackutils menu
+```
+
---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const colors = require('colors');
+
+const slackMenu = require('../lib/slackMenu');
+
+const channelService = require('../lib/channelService');
+const channelData = require('../lib/channelData');
+const channelNameQuestion = require('../lib/channelNameQuestion');
+
+program
+  .command('menu') 
+  .alias('su') 
+  .description('Slack Utilities') 
+  .action(function (args) {
+
+    slackMenu()
+    .then(function (answers) {
+      if (answers.selectedAction == 'Create Channels') {
+        var data = channelData.testClubs;
+        channelService.createChannels(data);
+      }
+      else if (answers.selectedAction == 'Delete Channel') {
+        channelNameQuestion()
+        .then(function(answers) {
+          channelService.deleteChannel(answers.channelName);
+        });
+      }
+    })
+
+  });
+
+// allow commander to parse `process.argv`
+program.parse(process.argv);
