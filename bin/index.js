4bf5c7af0cc0b557555e71aa7db172e57df274d2
Greg Mercer (GBear) <gmercer@stanford.edu>
2018-08-27 06:54:53 -0700

    Added new Create Groups and Invite to Group actions

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,12 @@ const channelService = require('../lib/channelService');
 const channelData = require('../lib/channelData');
 const channelNameQuestion = require('../lib/channelNameQuestion');
 
+const groupService = require('../lib/groupService');
+const groupData = require('../lib/groupData');
+const groupNameQuestion = require('../lib/groupNameQuestion');
+
+const userEmailQuestion = require('../lib/userEmailQuestion');
+
 program
   .command('menu') 
   .alias('su') 
@@ -31,6 +37,38 @@ program
         var data = channelData.testClubs;
         channelService.deleteChannels(data);
       }
+      else if (answers.selectedAction == 'Create Groups') {
+        var data = groupData.testClubs;
+        groupService.createGroups(data);
+      }
+      else if (answers.selectedAction == 'Invite to Group') {
+        let data = {};
+        groupNameQuestion()
+        .then(function(answers) {
+          data.groupName = answers.groupName;
+          answers = userEmailQuestion();
+          return answers;
+        })
+        .then(function(answers) {
+          data.userEmail = answers.userEmail;
+          groupService.inviteToGroup(data.groupName, data.userEmail);
+        })
+      }
+      // todo: Slack Dev support is saying that deleting groups
+      // is not possible with the current undocumented groups.delete 
+      // api. 
+      /*
+      else if (answers.selectedAction == 'Delete Group') {
+        groupNameQuestion()
+        .then(function(answers) {
+          groupService.deleteGroup(answers.groupName);
+        });
+      }
+      else if (answers.selectedAction == 'Delete Groups') {
+        let data = groupData.testClubs;
+        groupService.deleteGroups(data);
+      }
+      */
     })
 
   });
